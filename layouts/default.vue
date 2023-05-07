65a79309e29e2e2f01feafc13e6df5236e69dc46
Ali Azimi <alibabaazimi@gmail.com>
2023-05-07 23:20:59 +0200

    created home page

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="w-1/2 mx-auto bg-slate-100">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  title: 'Ali B. Azimi',
+  meta: [
+    {
+      name: 'description',
+      content: 'Ali Azimi is a software engineer based in Munich.'
+    }
+  ]
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLayout>
+    <div class="px-5 py-10">
+      <div>
+        <h1 class="text-3xl">Ali Azimi</h1>
+        <p class="py-5 text-md">
+          My name is Ali and I am a software engineer. I love to build things with code. I am currently working on a project called Negaa. It is a social media platform for people to share their items with others. I am also working on a project called Behina. It is a chrome extension that helps you focus and be more productive.
+        </p>
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
