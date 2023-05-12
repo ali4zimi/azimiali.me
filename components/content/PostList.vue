e5cb00a4eb86d18fe92b20c381f7f4c3c92842f7
Ali Azimi <alibabaazimi@gmail.com>
2023-05-12 12:10:29 +0200

    change images dir

--- components/content/PostList.vue
+++ components/content/PostList.vue
@@ -3,6 +3,7 @@
     <ContentList path="/posts" v-slot="{ list }">
       <div v-for="article in list" :key="article._path">
         <nuxt-link :to="article._path" class="text no-underline text-2xl mt-5">{{ article.title }}</nuxt-link>
+        <img :src="article.coverImage" class="w-full" />
       </div>
     </ContentList>
   </div>
