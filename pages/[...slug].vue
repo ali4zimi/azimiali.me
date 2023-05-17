6e3624de90ce4486765b7c4a4c02d9134521083d
Ali Azimi <alibabaazimi@gmail.com>
2023-05-17 06:05:20 +0200

    prose margin fixed; now it center itself

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLayout>
     <div class="max-w-3xl mx-auto mt-10 px-4">
-      <main class="prose dark:prose-invert">
+      <main class="prose mx-auto dark:prose-invert">
         <ContentDoc />
       </main>
     </div>
