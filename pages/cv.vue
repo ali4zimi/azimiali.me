7fbc853fcb7ae0b6718794b1265d890ccbb47998
Ali Azimi <alibabaazimi@gmail.com>
2023-05-28 06:35:58 +0200

    timeline fixed

--- pages/cv.vue
+++ pages/cv.vue
@@ -1,69 +1,81 @@
 <template>
   <NuxtLayout>
-    <h1 class="text-center">Coming Soon</h1>
-
-    <div class="dragzone hidden">
-      <div class="timeline-wrapper">
-        <div class="bg-black absolute top-0 bottom-0 w-1"></div>
-
-        <div class="pointer absolute bottom-0 left-0 z-10">
-          <div class="flex items-center gap-0">
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 15 15"><path fill="currentColor" d="M3 7.5L11 0v15L3 7.5Z"/></svg>
-            <div class="w-full border">1995</div>
-          </div>
+    <h1>Coming Soon</h1>
+    <div class="presentation-screen w-full h-[80vh] relative mt-5 bg-slate-50">
+      <div class="timeline-wrapper hidden">
+        <div class="timeline">
+          <div class="line"></div>
+          <div class="pointer">1995</div>
+          <input type="range" class="range" @input="movePointer" value="0" />
         </div>
-        <input type="range" list="tickmarks" @input="moveSlider" />
       </div>
     </div>
   </NuxtLayout>
 </template>
 
+
 <style>
 
-
-.dragzone {
-  @apply bg-slate-100;
-  position: relative;
-  width: 100%;
-  height: 80vh;
-}
-
 .timeline-wrapper {
-  @apply absolute top-0 bottom-0 bg-red-100;
+  @apply absolute py-[30px];
+  min-width: 50px;
+  height: 100%;
   left: 50%;
   transform: translateX(-50%);
 }
 
-
-input[type="range"] {
-  position: absolute;
-  top: 10px;
-  bottom: 10px;
-  z-index: 20;
-  cursor: pointer;
-  appearance: slider-vertical;
-  width: 40px;
-  opacity: 0;
+.timeline {
+  @apply relative cursor-pointer;
+  height: 100%;
 }
 
+.line {
+  @apply absolute  w-1 bg-green-400 rounded-md;
+  left: 50%;
+  transform: translateX(-50%);
+  top: 10px;
+  bottom: 10px;
+}
+
+.pointer {
+  @apply absolute w-4 h-4 bg-green-600 rounded-md text-white;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 100%;
+  height: auto;
+  text-align: center;
+}
+
+.range {
+  /* appearance: slider-vertical; */
+  -webkit-appearance: slider-vertical;
+  -moz-appearance: slider-vertical;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+}
 
 </style>
 
 <script setup>
 
-const moveSlider = (e) => {
-  const dragzone = document.querySelector('.dragzone');
+
+const movePointer = (e) => {
   const pointer = document.querySelector('.pointer');
+  const line = document.querySelector('.timeline');
 
-  const dragzoneHeight = dragzone.offsetHeight;
+  const pointerHeight = pointer.offsetHeight;
+  const lineHeight = line.offsetHeight;
 
-  // pointer.style.bottom = (e.target.value * (dragzoneHeight - 60) / e.target.offsetHeight) + '%';
+  const pointerPosition = e.target.value / 100;
+  const space = lineHeight - pointerHeight;
 
+  pointer.style.bottom = (pointerPosition * space) + 'px';
 
-  let a = e.target.value * 100 / e.target.offsetHeight;
-  let b = (dragzoneHeight - pointer.offsetHeight - 20 ) / 100;
-
-  pointer.style.bottom = (a * b) + '%';
+  console.log(pointerPosition * space);
 
 }
 </script>
