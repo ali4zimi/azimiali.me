3c8efff627e516795a4f97396f6d70a4aac3cb37
Ali Azimi <alibabaazimi@gmail.com>
2023-05-28 15:26:17 +0200

    new cv page created, old one changed to my-history

--- pages/cv.vue
+++ pages/cv.vue
@@ -1,13 +1,20 @@
 <template>
   <NuxtLayout>
-    <h1>Coming Soon</h1>
-    <div class="presentation-screen w-full h-[80vh] relative mt-5 bg-slate-50">
-      <div class="timeline-wrapper hidden">
-        <div class="timeline">
-          <div class="line"></div>
-          <div class="pointer">1995</div>
-          <input type="range" class="range" @input="movePointer" value="0" />
+    <h1 class="text-center">Coming Soon</h1>
+    <div class="relative w-full  p-0 sm:p-3 flex flex-col gap-5">
+
+      <div class="item" v-for="(item,index) in experiences">
+        <div class="col" :class="index%2==0 ? 'col-left' : 'invisible'">
+          <div class="font-bold">{{ item.title }}</div>
         </div>
+        <div class="pointer"></div>
+        <div class="col" :class="index%2==0 ? 'invisible' : 'col-right'">
+          <div class="font-bold">{{ item.title }}</div>
+        </div>
+      </div>
+
+      <div class="cv-timeline">
+        <div class="cv-timeline-filler"></div>
       </div>
     </div>
   </NuxtLayout>
@@ -16,66 +23,103 @@
 
 <style>
 
-.timeline-wrapper {
-  @apply absolute py-[30px];
-  min-width: 50px;
+.cv-timeline {
+  @apply w-2 h-full bg-slate-300 left-[10px] sm:left-[50%];
+  position: absolute;
+  width: 2px;
   height: 100%;
-  left: 50%;
+  top: 0;
   transform: translateX(-50%);
 }
 
-.timeline {
-  @apply relative cursor-pointer;
-  height: 100%;
+
+/* .cv-timeline-filler {
+  @apply w-full h-full bg-slate-500;
+} */
+
+.item {
+  @apply relative w-full pl-8 sm:px-0 flex justify-center gap-[50px];
 }
 
-.line {
-  @apply absolute  w-1 bg-green-400 rounded-md;
-  left: 50%;
-  transform: translateX(-50%);
-  top: 10px;
-  bottom: 10px;
+.col {
+  @apply w-full sm:w-1/3 h-80 p-4 bg-slate-50 rounded-md cursor-pointer;
+}
+
+.invisible {
+  @apply hidden sm:block;
+}
+
+.col:hover {
+  @apply bg-slate-100;
 }
 
 .pointer {
-  @apply absolute w-4 h-4 bg-green-600 rounded-md text-white;
-  bottom: 0;
-  left: 50%;
+  @apply w-5 h-5 top-5 absolute bg-slate-300 rounded-full z-20 cursor-pointer left-[10px] sm:left-[50%];
   transform: translateX(-50%);
-  width: 100%;
-  height: auto;
-  text-align: center;
 }
 
-.range {
-  /* appearance: slider-vertical; */
-  -webkit-appearance: slider-vertical;
-  -moz-appearance: slider-vertical;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  opacity: 0;
-  cursor: pointer;
+.col-left:hover + .pointer {
+  @apply bg-slate-500;
 }
 
+.pointer:has(+ .col-right:hover) {
+  @apply bg-slate-500;
+}
+
+.pointer:hover {
+  @apply bg-slate-500;
+}
+
+.pointer:hover + .col-right {
+  @apply bg-slate-100;
+}
 </style>
 
 <script setup>
 
+const experiences = [
+  {
+    title: 'Software Engineer',
+    company: 'Google',
+    date: '2019 - Present',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc.',
+  },
+  {
+    title: 'Web Developer',
+    company: 'Facebook',
+    date: '2017 - 2019',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc.',
+  },
+  {
+    title: 'Software Engineer',
+    company: 'Google',
+    date: '2019 - Present',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc.',
+  },
+  {
+    title: 'Web Developer',
+    company: 'Facebook',
+    date: '2017 - 2019',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc.',
+  },
+  {
+    title: 'Software Engineer',
+    company: 'Google',
+    date: '2019 - Present',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc.',
+  },
+  {
+    title: 'Web Developer',
+    company: 'Facebook',
+    date: '2017 - 2019',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc.',
+  },
+  {
+    title: 'Software Engineer',
+    company: 'Google',
+    date: '2019 - Present',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu'
+  }
+]
 
-const movePointer = (e) => {
-  const pointer = document.querySelector('.pointer');
-  const line = document.querySelector('.timeline');
-
-  const pointerHeight = pointer.offsetHeight;
-  const lineHeight = line.offsetHeight;
-
-  const pointerPosition = e.target.value / 100;
-  const space = lineHeight - pointerHeight;
-
-  pointer.style.bottom = (pointerPosition * space) + 'px';
-
-  console.log(pointerPosition * space);
-
-}
 </script>
