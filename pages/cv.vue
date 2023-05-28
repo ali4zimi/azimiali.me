e91a2acd25cf83054aa69ceb142680e7072c9704
Ali Azimi <alibabaazimi@gmail.com>
2023-05-29 00:13:25 +0200

    cv ui improved

--- pages/cv.vue
+++ pages/cv.vue
@@ -19,55 +19,42 @@
             </div>
   
             <div class="events flex flex-col mt-5">
-              <div class="item mt-5" v-for="(item,index) in experiences">
-                <div class="col" :class="index%2==0 ? 'col-left' : 'invisible'">
+              <div class="item mt-5" v-for="(item,index) in data.educations" :class="index%2!=0 ? 'flex-row-reverse' : ''">
+                <div class="col">
                   <div class="item-header w-full flex justify-between">
                     <div class="flex gap-2">
                       <div class="w-10 h-10  rounded-full flex justify-center items-center">
-                        <img :src="item.icon" alt="" />
+                        <img :src="item.organization.icon" />
                       </div>
                       <div>
                         <div class="font-bold">{{ item.title }}</div>
-                        <a href="https://www.google.com" class="text-sm">{{ item.company }}</a>
+                        <a :href="item.organization.website" class="text-sm">{{ item.organization.name }}</a>
                       </div>
                     </div>
                     <div>
-                      <div class="text-sm">{{ item.date }}</div>
+                      <div class="text-sm">{{ formatDate(item.start_date) }}</div>
                     </div>
                   </div>
       
                   <div class="item-body mt-3">
-                    <div class="">{{ item.description }}</div>
+                    <ContentRenderer :value="item">
+                      {{ item.description }}
+                    </ContentRenderer>
+                    
+                    <!-- <ContentRendererMarkdown :value="item.description" /> -->
+                    <!-- <div class="prose">{{ item.description }}</div> -->
                   </div>
       
                 </div>
                 <div class="pointer"><div class="pointer-circle"></div></div>
-                <div class="col" :class="index%2==0 ? 'invisible' : 'col-right'">
-                  <div class="item-header w-full flex justify-between">
-                    <div class="flex gap-2">
-                      <div class="w-10 h-10  rounded-full flex justify-center items-center">
-                        <img :src="item.icon" alt="" />
-                      </div>
-                      <div>
-                        <div class="font-bold">{{ item.title }}</div>
-                        <a href="https://www.google.com" class="text-sm">{{ item.company }}</a>
-                      </div>
-                    </div>
-                    <div>
-                      <div class="text-sm">{{ item.date }}</div>
-                    </div>
-                  </div>
-      
-                  <div class="item-body mt-3">
-                    <div class="">{{ item.description }}</div>
-                  </div>
+                <div class="col empty">
                 </div>
               </div>
             </div>
           </div>
         </div>
 
-                
+
         <div class="cv-timeline">
           <div class="tm-header">
             <div class="tm-title">
@@ -83,53 +70,36 @@
             </div>
   
             <div class="events flex flex-col mt-5">
-              <div class="item mt-5" v-for="(item,index) in experiences">
-                <div class="col" :class="index%2==0 ? 'col-left' : 'invisible'">
+              <div class="item mt-5" v-for="(item,index) in data.experiences" :class="index%2!=0 ? 'flex-row-reverse' : ''">
+                <div class="col">
                   <div class="item-header w-full flex justify-between">
                     <div class="flex gap-2">
                       <div class="w-10 h-10  rounded-full flex justify-center items-center">
-                        <img :src="item.icon" alt="" />
+                        <img :src="item.organization.icon" alt="" />
                       </div>
                       <div>
                         <div class="font-bold">{{ item.title }}</div>
-                        <a href="https://www.google.com" class="text-sm">{{ item.company }}</a>
+                        <a :href="item.organization.website" class="text-sm">{{ item.organization.name }}</a>
                       </div>
                     </div>
                     <div>
-                      <div class="text-sm">{{ item.date }}</div>
+                      <div class="text-sm">{{ formatDate(item.start_date) }}</div>
                     </div>
                   </div>
       
                   <div class="item-body mt-3">
-                    <div class="">{{ item.description }}</div>
+                    <div class="prose">{{ item.description }}</div>
                   </div>
       
                 </div>
                 <div class="pointer"><div class="pointer-circle"></div></div>
-                <div class="col" :class="index%2==0 ? 'invisible' : 'col-right'">
-                  <div class="item-header w-full flex justify-between">
-                    <div class="flex gap-2">
-                      <div class="w-10 h-10  rounded-full flex justify-center items-center">
-                        <img :src="item.icon" alt="" />
-                      </div>
-                      <div>
-                        <div class="font-bold">{{ item.title }}</div>
-                        <a href="https://www.google.com" class="text-sm">{{ item.company }}</a>
-                      </div>
-                    </div>
-                    <div>
-                      <div class="text-sm">{{ item.date }}</div>
-                    </div>
-                  </div>
-      
-                  <div class="item-body mt-3">
-                    <div class="">{{ item.description }}</div>
-                  </div>
+                <div class="col empty">
                 </div>
               </div>
             </div>
           </div>
         </div>
+
 
       </main>
     </div>
@@ -177,17 +147,12 @@
 }
 
 .col {
-  @apply w-full sm:w-1/3 h-80 p-4 bg-slate-50 dark:bg-slate-700 dark:text-slate-200 rounded-md cursor-pointer;
+  @apply w-full sm:w-1/3 min-h-[200px] max-h-[600px] p-4 bg-slate-50 dark:bg-slate-700 dark:text-slate-200 rounded-md cursor-pointer;
 }
 
-.invisible {
-  @apply hidden sm:block;
+.empty {
+  @apply hidden sm:block sm:invisible;
 }
-
-.col:hover {
-  @apply bg-slate-200 dark:bg-slate-600;
-}
-
 
 .pointer {
   @apply w-5 h-5 top-5 absolute
@@ -204,59 +169,34 @@
   transform: translate(-50%, -50%);
 }
 
+.col:hover {
+  @apply bg-slate-200 dark:bg-slate-600;
+}
+
+.col:hover + .pointer .pointer-circle {
+  @apply bg-slate-400 dark:bg-slate-200;
+}
+
 .pointer:hover .pointer-circle {
   @apply bg-slate-400 dark:bg-slate-200;
 }
 
-.col-left:hover + .pointer .pointer-circle {
-  @apply bg-slate-400 dark:bg-slate-200;
+.pointer:hover + .col {
+  @apply bg-slate-200 dark:bg-slate-600;
 }
 
-.pointer:has(+ .col-right:hover) {
-  @apply bg-slate-400 dark:bg-slate-200;
-}
-
-
-/* .pointer:hover + .col-right {
-  @apply bg-slate-100;
-} */
 </style>
 
 <script setup>
 
-const experiences = [
-  {
-    title: 'Software Engineer',
-    company: 'Google',
-    icon: 'https://img.icons8.com/color/48/000000/google-logo.png',
-    date: '2019 - Present',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc.',
-  },
-  {
-    title: 'Web Developer',
-    company: 'Facebook',
-    icon: 'https://img.icons8.com/color/48/000000/facebook-new.png',
-    date: '2017 - 2019',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc.',
-  },
-  {
-    title: 'IT Manager',
-    company: 'Microsoft',
-    icon: 'https://img.icons8.com/color/48/000000/microsoft.png',
-    date: '2019 - Present',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc.',
-  },
+const { data } = useAsyncData('cv', () => queryContent('pages/cv').findOne())
 
-]
 
-const educations = [
-  {
-    title: 'Master\'s Degree in Management and Technology',
-    organization: 'Technical University of Munich',
-    icon: 'https://img.icons8.com/color/48/000000/technical-support.png',
-    start_date: 'Oct 2022',
-    end_date: '',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc. Sed euismod, diam quis aliquam ultricies, nunc nisl ultrices odio, nec aliquam eros nisl eu nunc.',
-  }
-]
+const formatDate = (date) => {
+  // Month Year
+  const d = new Date(date)
+  return `${d.toLocaleString('default', { month: 'long' })} ${d.getFullYear()}`
+}
+
+
 </script>
