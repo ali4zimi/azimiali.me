170193301d2bcacba39911c493179fc8275c6149
ali4zimi <alibabaazimi@gmail.com>
2023-06-23 01:28:01 +0200

    added component "more-details" for hiding details

---
+++ components/content/MoreDetails.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+const show = ref(false);
+
+function expand() {
+    const details = document.querySelector('.details');
+    details.style.maxHeight = '1000px';
+    show.value = true;
+}
+
+function collapse() {
+    const details = document.querySelector('.details');
+    details.style.maxHeight = '0px';
+    show.value = false;
+}
+
+
+</script>
+
+<template>
+    <div>
+        <button v-if="!show" @click="expand()">Show More...</button>
+        <button v-if="show" @click="collapse()">Show Less...</button>
+        <div class="details">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+button {
+    @apply text-slate-400 text-sm;
+}
+.details {
+    max-height: 0;
+    overflow: hidden;
+    transition: max-height 0.35s ease-out;
+}
+</style>
