c3658b14d759331abc1835dd39c91f9e0499e0dc
Ali Baba Azimi <azimi.ali@tum.de>
2026-02-08 18:29:08 +0100

    chore: add enviromental variables to use notion as database

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,12 @@ export default defineNuxtConfig({
     '~/assets/css/main.css',
   ],
 
+  runtimeConfig: {
+    notionApiKey: "",
+    notionPostsDsId: "",
+    notionVisitorsDsId: "",
+  },
+
   content: {
     markdown: {
       toc: {
