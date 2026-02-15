2f2bd64fbcfdaa4538fa8acb314bc21456a78f67
Ali Baba Azimi <azimi.ali@tum.de>
2026-02-15 02:36:47 +0100

    updated navbar, and added bottom margin to the projects page

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -23,9 +23,9 @@
           <div class="inline-block text-slate-600 top-0 text-2xl dark:text-slate-300">A</div>
           <div class="inline-block text-slate-400 text-md dark:text-slate-400">A</div>
         </nuxt-link>
-        <!-- <div class="loading-icon hidden">
+        <div class="loading-icon hidden">
           <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><path fill="currentColor" d="M10.72,19.9a8,8,0,0,1-6.5-9.79A7.77,7.77,0,0,1,10.4,4.16a8,8,0,0,1,9.49,6.52A1.54,1.54,0,0,0,21.38,12h.13a1.37,1.37,0,0,0,1.38-1.54,11,11,0,1,0-12.7,12.39A1.54,1.54,0,0,0,12,21.34h0A1.47,1.47,0,0,0,10.72,19.9Z"><animateTransform attributeName="transform" dur="0.75s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"/></path></svg>
-        </div> -->
+        </div>
       </div>
     </div>
 
@@ -131,17 +131,17 @@ const links = [
     path: '/',
   },
   // {
-  //   name: 'CV',
-  //   path: '/cv',
+  //   name: 'Projects',
+  //   path: '/projects',
   // },
   {
-    name: 'Portfolio',
-    path: '/portfolio',
+    name: 'Open Source',
+    path: '/open-source',
   },
-  /* {
-    name: 'Blog',
-    path: '/blog',
-  }, */
+  // {
+  //   name: 'Blog',
+  //   path: '/blog',
+  // }, 
 ]
 
 const theme = useTheme();
