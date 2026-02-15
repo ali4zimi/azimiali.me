ef16e761f947594608f3e03adb16f048729c53fd
Ali Baba Azimi <azimi.ali@tum.de>
2026-02-16 00:09:47 +0100

    improved progressbar

--- components/ProgressBar.vue
+++ components/ProgressBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="loader"></div>
+  <div class="loader" :style="{ width: progress + '%' }"></div>
 </template>
 
 <style>
@@ -8,28 +8,79 @@
   top: 0;
   left: 0;
   height: 3px;
-  background-color: #acaeac;
+  background: linear-gradient(to right, #64748b, #475569);
   z-index: 9999;
-  transition: width 0.3s ease-in-out;
+  transition: width 0.2s ease-out, opacity 0.4s ease-in-out;
   width: 0%;
+  opacity: 1;
+}
+
+.loader.hidden {
+  opacity: 0;
 }
 </style>
 
 <script setup>
 const router = useRouter()
+const progress = ref(0)
+let interval = null
 
-router.beforeEach(() => {
-  const loader = document.querySelector(".loader");
-  loader.classList.remove("hidden");
-  loader.style.width = "10%";
+const startProgress = () => {
+  progress.value = 0
+  const loader = document.querySelector(".loader")
+  if (loader) {
+    loader.classList.remove("hidden")
+  }
+  
+  // Simulate realistic loading progress
+  let currentProgress = 0
+  interval = setInterval(() => {
+    if (currentProgress < 20) {
+      currentProgress += Math.random() * 10
+    } else if (currentProgress < 50) {
+      currentProgress += Math.random() * 4
+    } else if (currentProgress < 80) {
+      currentProgress += Math.random() * 2
+    } else if (currentProgress < 90) {
+      currentProgress += Math.random() * 0.5
+    }
+    
+    currentProgress = Math.min(currentProgress, 90)
+    progress.value = currentProgress
+  }, 200)
+}
+
+const finishProgress = () => {
+  if (interval) {
+    clearInterval(interval)
+    interval = null
+  }
+  
+  progress.value = 100
+  
+  setTimeout(() => {
+    const loader = document.querySelector(".loader")
+    if (loader) {
+      loader.classList.add("hidden")
+    }
+    setTimeout(() => {
+      progress.value = 0
+    }, 400)
+  }, 300)
+}
+
+router.beforeEach((to, from, next) => {
+  startProgress()
+  next()
 })
 
 router.afterEach(() => {
-  const loader = document.querySelector(".loader");
-  loader.style.width = "100%";
-  setTimeout(() => {
-    loader.classList.add("hidden");
-    loader.style.width = "0%";
-  }, 1000);
+  finishProgress()
+})
+
+onUnmounted(() => {
+  if (interval) {
+    clearInterval(interval)
+  }
 })
 </script>
